8305b8aefc0f6853cade604e99629ab498782ae1
Vydro <duvergervalentin.pro@gmail.com>
2019-03-05 16:22:16 +0100

    form connexion/saisirunevisite + images

---
+++ Liemie/Connexion.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Liemie
+{
+    partial class Connexion
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Connexion));
+            this.lbl_identifiant = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_motDePasse = new System.Windows.Forms.TextBox();
+            this.tb_identifiant = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_identifiant
+            // 
+            this.lbl_identifiant.AutoSize = true;
+            this.lbl_identifiant.Location = new System.Drawing.Point(184, 230);
+            this.lbl_identifiant.Name = "lbl_identifiant";
+            this.lbl_identifiant.Size = new System.Drawing.Size(53, 13);
+            this.lbl_identifiant.TabIndex = 0;
+            this.lbl_identifiant.Text = "Identifiant";
+            this.lbl_identifiant.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mot de passe :";
+            // 
+            // tb_motDePasse
+            // 
+            this.tb_motDePasse.Location = new System.Drawing.Point(267, 259);
+            this.tb_motDePasse.Name = "tb_motDePasse";
+            this.tb_motDePasse.Size = new System.Drawing.Size(100, 20);
+            this.tb_motDePasse.TabIndex = 2;
+            // 
+            // tb_identifiant
+            // 
+            this.tb_identifiant.Location = new System.Drawing.Point(267, 227);
+            this.tb_identifiant.Name = "tb_identifiant";
+            this.tb_identifiant.Size = new System.Drawing.Size(100, 20);
+            this.tb_identifiant.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(187, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(190, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 378);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tb_identifiant);
+            this.Controls.Add(this.tb_motDePasse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_identifiant);
+            this.Name = "Menu";
+            this.Text = "Connexion";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_identifiant;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_motDePasse;
+        private System.Windows.Forms.TextBox tb_identifiant;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Liemie/SaisirUneVisite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Liemie
+{
+    public partial class SaisirUneVisite : Form
+    {
+        public SaisirUneVisite()
+        {
+            InitializeComponent();
+        }
+    }
+}
